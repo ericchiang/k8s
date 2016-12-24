d317e053a8c7ba94aa12f9bed69ab3c482920af9
Eric Chiang <eric.chiang.m@gmail.com>
2016-12-23 18:00:03 -0800

    *: add JSON marshaling methods for time to support ThirdPartyResources

--- gen.sh
+++ gen.sh
@@ -39,3 +39,36 @@ sed -i 's|"k8s.io.kubernetes.pkg.|"github.com/ericchiang.k8s.|g' $( find {api,ap
 rm -rf assets
 
 go run gen.go
+
+cat << EOF >> api/unversioned/time.go
+package unversioned
+
+import (
+    "encoding/json"
+    "time"
+)
+
+func (t Time) MarshalJSON() ([]byte, error) {
+    var seconds, nanos int64
+    if t.Seconds != nil {
+        seconds = *t.Seconds
+    }
+    if t.Nanos != nil {
+        nanos = int64(*t.Nanos)
+    }
+    return json.Marshal(time.Unix(seconds, nanos))
+}
+
+func (t *Time) UnmarshalJSON(p []byte) error {
+    var t1 time.Time
+    if err := json.Unmarshal(p, &t1); err != nil {
+        return err
+    }
+    seconds := t1.Unix()
+    nanos := int32(t1.UnixNano())
+    t.Seconds = &seconds
+    t.Nanos = &nanos
+    return nil
+}
+EOF
+gofmt -w api/unversioned/time.go
