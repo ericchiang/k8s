8e3f6e141c77fa38cfcf1bbaca55d7045aa7e73f
Tom Booth <tombooth@gmail.com>
2017-04-20 15:11:40 +0100

    LabelSelector should support / chars in keys
    Kubernetes uses / characters in its applied labels, for example the
    label applied to a node to determine whether it is a node or master.
    This library should support using this character in a key name.

--- labels_test.go
+++ labels_test.go
@@ -15,6 +15,12 @@ func TestLabelSelector(t *testing.T) {
 		},
 		{
 			f: func(l *LabelSelector) {
+				l.Eq("kubernetes.io/role", "master")
+			},
+			want: "kubernetes.io/role=master",
+		},
+		{
+			f: func(l *LabelSelector) {
 				l.In("type", "prod", "staging")
 				l.Eq("component", "frontend")
 			},
