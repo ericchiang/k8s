e7a4ab0a52d7872d55ddaa52605fb5b115060669
Luke Shumaker <lukeshu@datawire.io>
2019-02-04 17:13:16 -0500

    Watcher: Correctly handle .Type==&"ERROR" watch events
    If a watch sends back
    
        {"type":"ERROR","object":{"kind":"Status","apiVersion":"v1","metadata":{},"status":"Failure","message":"too old resource version: 500000 (557576)","reason":"Gone","code":410}}
    
    instead of returning opaque errors from proto
    
        proto: wrong wireType = 6 for field ServiceAccountName
    
    it should return an APIError:
    
        kubernetes api: Failure 410 too old resource version: 500000 (557287)

--- watch.go
+++ watch.go
@@ -63,6 +63,16 @@ func (w *watcherJSON) Next(r Resource) (string, error) {
 	if event.Type == "" {
 		return "", errors.New("watch event had no type field")
 	}
+	if event.Type == EventError {
+		status := &metav1.Status{}
+		if err := json.Unmarshal([]byte(event.Object), status); err != nil {
+			return "", fmt.Errorf("decoding event error: %v", err)
+		}
+		return event.Type, &APIError{
+			Status: status,
+			Code:   int(*status.Code),
+		}
+	}
 	if err := json.Unmarshal([]byte(event.Object), r); err != nil {
 		return "", fmt.Errorf("decode resource: %v", err)
 	}
@@ -84,6 +94,16 @@ func (w *watcherPB) Next(r Resource) (string, error) {
 	}
 	if event.Type == nil || *event.Type == "" {
 		return "", errors.New("watch event had no type field")
+	}
+	if *event.Type == EventError {
+		status := &metav1.Status{}
+		if err := proto.Unmarshal(unknown.Raw, status); err != nil {
+			return "", fmt.Errorf("decoding event error: %v", err)
+		}
+		return *event.Type, &APIError{
+			Status: status,
+			Code:   int(*status.Code),
+		}
 	}
 	if err := proto.Unmarshal(unknown.Raw, msg); err != nil {
 		return "", err
